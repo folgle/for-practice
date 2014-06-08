c3b279378cb94ef896f346a3a1d4ac44e45de802
Masashi Iwao <iwao.masashi@gmail.com>
2014-06-08 15:59:27 +0900

    modified chapter 3

--- 3/spa/js/spa.js
+++ 3/spa/js/spa.js
@@ -9,14 +9,9 @@
   white: true
 */
 /*global $, spa:true */
-
 var spa = (function() {
   var initModule = function($container) {
-    $container.html(
-      '<h1 style="display:inline-block; margin:25px;">'
-      + 'hello world!'
-      + '</h1>'
-    );
+    spa.shell.initModule( $container);
   };
 
   return {initModule: initModule};
